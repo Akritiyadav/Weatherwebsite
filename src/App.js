2d0033020313d36c366382ad56ded14ee4fcfb1c
“Akriti <akritiyadav895@gmail.com>
2022-09-19 10:03:07 +0530

    reactapp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./App.css";
+import Akriti from "./Components/Akriti";
+
+
+function App () {
+  return (
+    <>
+    <Akriti/>
+    </>
+  )
+}
+export default App;
